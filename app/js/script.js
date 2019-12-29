afff371ae2ced35f333ed89416ee6c979cd36f8f
Daniel <danielnaizabekov@gmail.com>
2019-12-30 00:51:14 +0600

    added header and posts part

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,14 @@
 $(function() {
-	console.log('Hello world');
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		vertical: true,
+		verticalSwiping: true,
+		dots: true
+	});
+
+	$('.slick-next').html('&darr;');
+	$('.slick-prev').html('&uarr;');
+	$('.slick-dots li').html('');
 });
