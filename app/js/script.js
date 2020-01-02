14e44bb7bdf03cac0ce9e8f7b7305418a861f5b5
Daniel <danielnaizabekov@gmail.com>
2020-01-02 20:46:35 +0600

    added adaptive footer and detail post

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,19 @@
 $(function() {
+	//===stopPropogations===
+	$('.menu, .header-search-wrap input').on('click', function(event) {
+		event.stopPropagation();
+	});
+	//===stopPropogations===
+
+	//===removeActiveStyles===
+	$('body').on('click', function() {
+		$('.menu').removeClass('menu-active');
+		$('.header-search-wrap').removeClass('search-input-active');
+	});
+	//===removeActiveStyles===
+
+
+
 	//===menu===
 	$('.menu__btn').on('click', function() {
 		$('.menu').toggleClass('menu-active');
@@ -7,27 +22,63 @@ $(function() {
 
 
 
-	//===slider===
+	//===header-search===
+	$('.header-search-form').on('submit', function(event) {
+		event.preventDefault();
+
+		let headerSearchValue = $(this).find('input').val();
+
+		if (headerSearchValue) {
+			console.log(headerSearchValue);
+			$(this)[0].reset();
+		}
+	});
+
+	$('.header-search-btn').on('click', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+
+		let headerSearchValue = $(this).parent().find('input').val();
+		$(this).parent().toggleClass('search-input-active');
+
+		if(headerSearchValue) {
+			console.log(headerSearchValue);
+			$(this).parent().find('form')[0].reset();
+		}
+	});
+	//===header-search===
+
+
+
+	//===header slider===
 	$('.slider').slick({
 		infinite: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		vertical: true,
 		verticalSwiping: true,
-		dots: true
+		dots: true,
+		responsive: [
+		{
+			breakpoint: 600,
+			settings: {
+				dots: false
+			}
+		}
+		]
 	});
 
 	$('.slick-next').html('&darr;');
 	$('.slick-prev').html('&uarr;');
 	$('.slick-dots li').html('');
-	//===slider===
+	//===header slider===
 
 
 
 	//===categories===
 	$('.categories-title, .menu-categories').on('click', function(event) {
 		event.preventDefault();
-		$(this).toggleClass('.categories-title-active');
+		$(this).toggleClass('categories-title-active');
 		$(this).parent().find('.categories-list').slideToggle(200);
 	})
 	//===categories===
